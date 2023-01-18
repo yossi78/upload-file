a41f1d6057ce92fb1a9b6074e27e7cd1fd8275a7
yossi78 <yossi78@gmail.com>
2023-01-18 12:16:52 +0200

    Edit test2

--- src/main/java/com/example/uploadfile/api/UploadFileController.java
+++ src/main/java/com/example/uploadfile/api/UploadFileController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-// test
+// YES
 @RestController
 @RequestMapping(value = "/upload")
 @Slf4j
